2a32133f111e4c100f976764c094b01799b82133
ramymaghraby <ramymaghraby@gmail.com>
2025-12-20 18:44:58 +0200

    Add project files.

---
+++ LMS.Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LMS.Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
